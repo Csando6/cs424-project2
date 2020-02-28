aa777aa900f0dfd7873ab445426ce2d8115206fc
Matt Jankowski <35581471+mjanko5@users.noreply.github.com>
2020-02-27 18:10:48 -0600

    created basic layout of code

--- app.R
+++ app.R
@@ -15,3 +15,50 @@ data <- read.csv(file = 'cleaned_hurricane_data.csv', sep = ",", header = TRUE)
 
 
 #project code here...
+
+#SHINY DASHBOARD
+
+# Create the shiny dashboard
+ui <- dashboardPage(
+  dashboardHeader(title = "Hurricane Data Analysis"),
+  dashboardSidebar(disable = FALSE, collapsed = FALSE,
+                   
+  #Sidebar input                 
+
+  ),
+  
+  #Body
+  dashboardBody(
+    
+      #put fluidRows and columns here
+
+  ))
+
+
+server <- function(input, output) {
+  
+  # increase the default font size
+  theme_set(theme_grey(base_size = 18))
+  
+  
+  #GET REACTIVE DATA FROM INPUT:
+  #(from P1):
+  
+            # calculate the values one time and re-use them in multiple charts to speed things up
+            # <- reactive({subset(filtered, filtered$username == input$Picker)}) #contatins just the data about selected picker
+            
+            
+            # observeEvent(input$Picker, {
+            #   print(paste0("You have chosen: ", input$Picker))
+            # })
+  
+  
+  
+  #PLOT THE DATA:
+  
+  #insert data components here
+  
+}
+
+shinyApp(ui = ui, server = server)
+
